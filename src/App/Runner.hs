08c0e880e000b948b88a07ff89df6324f5e47624
Marco Tarantino <taran.marco@protonmail.com>
2021-06-13 15:38:57 +0200

    Add logs with Trace effect

--- src/App/Runner.hs
+++ src/App/Runner.hs
@@ -8,21 +8,23 @@ import Data.IORef
 import Data.Proxy
 import Polysemy
 import Polysemy.AtomicState
+import Polysemy.Trace
 import Servant.Server
 
 data Config = Config
-    { cInitialState :: IORef AppState
-    }
+  { cInitialState :: IORef AppState
+  }
 
 createApp :: Config -> Application
 createApp config = serve (Proxy @StatusAPI) (liftServer config)
 
 liftServer :: Config -> ServerT StatusAPI Handler
 liftServer config = hoistServer (Proxy @StatusAPI) (interpretServer config) statusServer
-  where
-    interpretServer c sem =
-        sem
-            & runAtomicStateIORef (cInitialState c)
-            & runM
-            & liftToHandler
-    liftToHandler = Handler . ExceptT . fmap Right
+ where
+  interpretServer c sem =
+    sem
+      & runAtomicStateIORef (cInitialState c)
+      & traceToIO
+      & runM
+      & liftToHandler
+  liftToHandler = Handler . ExceptT . fmap Right
--- src/App/StateManager.hs
+++ src/App/StateManager.hs
@@ -5,6 +5,7 @@ module App.StateManager where
 import App.State
 import Polysemy
 import Polysemy.AtomicState
+import Polysemy.Trace
 
 data StateManager r a where
     Start :: StateManager r AppState
@@ -12,19 +13,28 @@ data StateManager r a where
 
 makeSem ''StateManager
 
-type StateConstraints r = (Member (AtomicState AppState) r, Member (Embed IO) r)
+type StateConstraints r = (Member Trace r, Member (AtomicState AppState) r, Member (Embed IO) r)
 
-turnOn :: (Member (AtomicState AppState) r, Member (Embed IO) r) => Sem r AppState
+turnOn :: (Member Trace r, Member (AtomicState AppState) r, Member (Embed IO) r) => Sem r AppState
 turnOn = do
-    s <- embed newOnApp
-    atomicPut s
-    pure s
+    trace "Called turnOn"
+    sOrig <- atomicGet
+    case sOrig of
+        AppState (AppOn _) -> do
+            trace "Already ON, nothing to do"
+            pure sOrig
+        AppState AppOff -> do
+            trace "Found app OFF, turning ON"
+            s <- embed newOnApp
+            atomicPut s
+            pure s
 
-turnOff :: (Member (AtomicState AppState) r) => Sem r AppState
+turnOff :: (Member Trace r, Member (AtomicState AppState) r) => Sem r AppState
 turnOff = do
+    trace "Called turnOff"
     let s = AppState AppOff
     atomicPut s
     pure s
 
-showState :: (Member (AtomicState AppState) r) => Sem r AppState
-showState = atomicGet
+showState :: (Member Trace r, Member (AtomicState AppState) r) => Sem r AppState
+showState = trace "Called showState" >> atomicGet
